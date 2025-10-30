f85d385760588daffa2ccec1017e29250600f6af
Nikita <nikitoshololeenko18@gmail.com>
2025-10-31 00:32:41 +0300

    add object method, add for cycle

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,21 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I learn how to commit my code");
+
+        for(int i = 0; i < 11; i++){
+            System.out.println(i);
+        }
+
+
+        int i;
+
+
+    }
+
+    public Object newObject(){
+        return object();
+    }
+    public Object object(){
+        return new Object();
     }
 }
