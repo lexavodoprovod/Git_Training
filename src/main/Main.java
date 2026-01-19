101fda949c499e6daa4f5866c7ff87b23370926b
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-01-19 17:31:17 +0300

    create justLikeThat

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,11 @@ public class Main {
         int i;
 
         System.out.println("Hello dev branch");
+        
+        for(int i = 0; i < 3; i++){
+            System.out.println("Just like that");
+        }
+
     }
 
     public Object newObject(){
